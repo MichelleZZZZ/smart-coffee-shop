2b1086b8a18c5b9974f901ec736dd61feac5a971
Michelle-Akqa <michelle.zhong@akqa.com>
2025-10-04 16:49:35 +1300

    add apollo client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
-import { Button } from "@/components/ui/button";
+import client from "@/lib/apollo-client";
+import { gql } from "@apollo/client";
 
-export default function Home() {
+export default async function BlogPage() {
+  const { data } = await client.query({
+    query: gql`
+      query GetBlogPosts {
+        blogPostCollection {
+          items {
+            title
+            slug
+          }
+        }
+      }
+    `,
+  });
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-      <Button variant="secondary">Click me</Button>
-    </h1>
-  )
+    <main className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Blog</h1>
+      <ul className="space-y-2">
+        {data.blogPostCollection.items.map((post: any) => (
+          <li key={post.slug} className="text-blue-600">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
