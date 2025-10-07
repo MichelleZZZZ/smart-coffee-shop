b4f98da32cd9ed605c84851e38a9bdf39e776ecc
Michelle-Akqa <michelle.zhong@akqa.com>
2025-10-07 14:46:59 +1300

    fix type issue

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { gql } from "@apollo/client"
 import client from "@/lib/apollo-client"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { Document } from "@contentful/rich-text-types"
 
 const GET_POST_BY_SLUG = gql`
   query GetPostBySlug($slug: String!) {
@@ -24,6 +25,7 @@ type BlogPost = {
   }
 }
 
+
 type QueryResponse = {
     blogPostCollection: {
       items: BlogPost[]
@@ -36,6 +38,8 @@ export default async function BlogPostPage({ params }: { params: { slug: string 
 
   const post = result.data?.blogPostCollection?.items[0]
 
+  console.log(post)
+
   if (!post) {
     return <p className="p-6">Post not found.</p>
   }
@@ -44,7 +48,9 @@ export default async function BlogPostPage({ params }: { params: { slug: string 
     <main className="p-6">
       <h1 className="text-3xl font-bold mb-2">{post.title}</h1>
       <p className="text-sm text-gray-500 mb-6">{new Date(post.publishDate).toDateString()}</p>
-      <article className="prose">{documentToReactComponents(post.body.json)}</article>
+        <article className="prose">
+            {post.body?.json ? 
+          documentToReactComponents(post.body.json) : ''}</article>
     </main>
   )
 }
